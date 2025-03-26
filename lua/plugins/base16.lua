b0bc11586452e5f4074b28628d66b82139e057f3
jaimebeneytez <me@jaimebeneytez.com>
2025-03-26 11:23:22 +0000

    colors: adjusted numbers anbd dividers

--- lua/plugins/base16.lua
+++ lua/plugins/base16.lua
@@ -14,40 +14,50 @@ return {
 		})
 
 		local km = vim.keymap -- for conciseness
+		local set_hl = vim.api.nvim_set_hl
+		local grey = '#2f4f4f'
+
+		vim.cmd([[colorscheme base16-solarized-dark]])
+
+		-- Enable line numbers
+		vim.opt.number = true
+		vim.opt.cursorline = true
 
 		km.set('n', '<leader>dark', ':colorscheme base16-solarized-dark<cr>', { silent = true })
 		km.set('n', '<leader>light', ':colorscheme base16-solarized-light<cr>', { silent = true })
 
-		vim.cmd([[colorscheme base16-solarized-dark]])
-		vim.cmd([[hi VertSplit guifg=#002b36]])
-
-		vim.api.nvim_set_hl(0, '@lsp.type.function', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.class', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.decorator', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.enum', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.enumMember', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.function', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.interface', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.macro', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.method', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.namespace', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.parameter', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.property', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.struct', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.type', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.typeParameter', {})
-		vim.api.nvim_set_hl(0, '@lsp.type.variable', {})
+		-- vim.cmd([[hi VertSplit guifg=#002b36]])
+		set_hl(0, 'WinSeparator', { fg = grey })
+		set_hl(0, 'LineNr', { fg = grey })
+		set_hl(0, 'CursorLine', { bg = '#002b36' })
+		set_hl(0, 'CursorLineNr', { fg = '#93a1a1', bold = true })
+		set_hl(0, 'SignColumn', { bg = 'none' })
+		set_hl(0, '@lsp.type.function', {})
+		set_hl(0, '@lsp.type.class', {})
+		set_hl(0, '@lsp.type.decorator', {})
+		set_hl(0, '@lsp.type.enum', {})
+		set_hl(0, '@lsp.type.enumMember', {})
+		set_hl(0, '@lsp.type.interface', {})
+		set_hl(0, '@lsp.type.macro', {})
+		set_hl(0, '@lsp.type.method', {})
+		set_hl(0, '@lsp.type.namespace', {})
+		set_hl(0, '@lsp.type.parameter', {})
+		set_hl(0, '@lsp.type.property', {})
+		set_hl(0, '@lsp.type.struct', {})
+		set_hl(0, '@lsp.type.type', {})
+		set_hl(0, '@lsp.type.typeParameter', {})
+		set_hl(0, '@lsp.type.variable', {})
 
 		local color = base16.colors
 
-		vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = '#002b36', fg = '#999999' })
-		vim.api.nvim_set_hl(0, 'TelescopeBorder', { link = 'TelescopeNormal' })
-		vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { link = 'TelescopeNormal' })
-		vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { link = 'TelescopeNormal' })
-		vim.api.nvim_set_hl(0, 'TelescopePromptPrefix', { link = 'TelescopeNormal' })
-		vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { link = 'TelescopeNormal' })
-		vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', { link = 'TelescopeNormal' })
-		vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', { link = 'TelescopeNormal' })
+		set_hl(0, 'TelescopeNormal', { bg = '#002b36', fg = '#999999' })
+		set_hl(0, 'TelescopeBorder', { link = 'TelescopeNormal' })
+		set_hl(0, 'TelescopePromptBorder', { link = 'TelescopeNormal' })
+		set_hl(0, 'TelescopePromptNormal', { link = 'TelescopeNormal' })
+		set_hl(0, 'TelescopePromptPrefix', { link = 'TelescopeNormal' })
+		set_hl(0, 'TelescopePromptTitle', { link = 'TelescopeNormal' })
+		set_hl(0, 'TelescopeResultsTitle', { link = 'TelescopeNormal' })
+		set_hl(0, 'TelescopePreviewTitle', { link = 'TelescopeNormal' })
 	end
 
 }
